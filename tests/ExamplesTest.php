fcf0232a803edfc30a812b99e39b2afd2a2391f6
Flavio Heleno <flaviohbatista@gmail.com>
2020-10-21 09:04:09 -0300

    Added ExamplesTest to ensure examples work throughout lib changes

---
+++ tests/ExamplesTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace League\Tactician\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExamplesTest extends TestCase
+{
+
+    /**
+     * @dataProvider exampleFiles
+     */
+    public function testExample(string $file, array $expect) : void
+    {
+        // executes the example script using the php binary
+        exec(
+            implode(
+                ' ',
+                [
+                    '/usr/bin/env',
+                    'php',
+                    '-f',
+                    escapeshellarg(__DIR__ . "/../examples/{$file}")
+                ]
+            ),
+            $output,
+            $exitCode
+        );
+
+        $this->assertSame(0, $exitCode);
+        $this->assertSame($expect, $output);
+    }
+
+    public function exampleFiles() : array
+    {
+        return [
+            [
+                '1-beginner-standard-usage.php',
+                ['User alice@example.com was registered!']
+            ],
+            [
+                '2-intermediate-create-middleware.php',
+                [
+                    'LOG: Starting RegisterUser',
+                    'User alice@example.com was registered!',
+                    'LOG: RegisterUser finished without errors'
+                ]
+            ],
+            [
+                '3-intermediate-custom-naming-conventions.php',
+                ['See, Tactician now calls the handle method we prefer!']
+            ],
+            [
+                '4-conditional-handlers.php',
+                [
+                    'Dispatched MyExternalCommand!',
+                    'User alice@example.com was registered!'
+                ]
+            ]
+        ];
+    }
+}
